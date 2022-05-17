70b7f8d1e0bfea53db7bf5f622d9841ef0f75014
Sidi Hmety <sidi.ehmety@gmail.com>
2022-05-17 12:01:40 +0000

    Correction des description ....

--- acpec_l10n_mr/__manifest__.py
+++ acpec_l10n_mr/__manifest__.py
@@ -13,9 +13,9 @@
     'website': 'https://www.acpec-sarl.com',
     'depends': ['base', 'account'],
     'description': u"""
-    Ce module charge un modèle de plan de comptable général pour la Mauritanie.
+    Ce module charge un modèle de plan de comptes généraux pour la Mauritanie.
     Il offre un ensemble réduit de comptes et de taxes suffisant pour démarrer avec ODOO. Instructions d'installation:
-    Créer une base Odoo vierge. Installer ce module et puis installer le module de la comptabilité""",
+    Créer une base Odoo vierge. Installer ce module et puis installer le module de la comptabilité. Et c'est tout!""",
     'data': [
         'data/l10n_mr_chart_data.xml',
         'data/account_chart_template_data.xml',
